8ea29d8a44fd227ea7d8bad1c4397449de553414
YoungGary <599045318@qq.com>
2020-01-20 16:58:50 +0800

    datepicker

--- examples/App.vue
+++ examples/App.vue
@@ -2,12 +2,8 @@
 
 <template>
   <div id="app">
-   
- 
-     <div class="margintop">
-      <fa-input type="textarea" :autosize="{ min:'200', max:'400'}"  placeholder="制定了3"  v-model="large"></fa-input>
-    </div>
-  
+   <fa-date-picker v-model="date"></fa-date-picker>
+   <fa-button>123</fa-button>  
   </div>
 </template>
 <script>
@@ -18,26 +14,11 @@ export default {
   },
   data(){
     return{
-      large:''
+      date: new Date()
     }
   },
   methods:{
-    toast(){
-      this.$showToast({
-        message:'错误1111111111111',
-        type:'success',
-        closeTime:3000
-      })
-    },
-    click(){
-      console.log(111)
-    },
-    show(){
-      this.$showLoading();
-      setTimeout(()=>{
-        this.$hideLoading();
-      },2000)
-    }
+
   }
 }
 </script>
@@ -47,8 +28,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /* text-align: center; */
+  /* color: #2c3e50; */
   min-height: 2000px;
   width: 100%
 }
---
+++ packages/datePicker/index.js
@@ -0,0 +1,7 @@
+
+import datePicker from './src/index'
+datePicker.install = function (Vue) {
+    Vue.component(datePicker.name, datePicker)
+}
+
+export default datePicker
